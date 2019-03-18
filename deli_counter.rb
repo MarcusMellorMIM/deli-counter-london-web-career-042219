b6425b1b96a1cfbe94d8238d03852ea2ab82ef5b
Marcus Mellor <marcus@mellor.com>
2019-03-18 13:37:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,4 +7,14 @@ def take_a_number(katz_deli, name)
 end
 
 def line( katz_deli )
-  puts "The line is currenty: #{katz_deli.each_index|x| print x+1". "katz_deli[x]}}
+  puts "The line is currenty: #{katz_deli.each_index{|x|print x+1". "katz_deli[x]}}
+end
+
+def take_a_number(katz_deli, name)
+  puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
+end
+
+def now_serving(katz_deli)
+  puts "Currently serving #{katz_deli.shift}."
+  katz_deli
+end

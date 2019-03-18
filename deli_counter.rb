97111c3eac7316a1e72c7388643a588d44547eb0
Marcus Mellor <marcus@mellor.com>
2019-03-18 13:28:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+katz_deli = []
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.count}"
+end
+
+def line( katz_deli )
+  puts "The line is currenty:"

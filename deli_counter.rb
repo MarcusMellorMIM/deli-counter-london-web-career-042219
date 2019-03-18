e556eb180ab308cb24affb5c0b1277d7fbaf0c97
Marcus Mellor <marcus@mellor.com>
2019-03-18 14:06:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,15 +3,21 @@ katz_deli = []
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.count}"
+  puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
+  katz_deli
 end
 
 def line( katz_deli )
-  puts "The line is currenty: #{katz_deli.each_index{|x|print x+1. katz_deli[x]}}"
-end
-
-def take_a_number(katz_deli, name)
-  puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
+  counter=0
+  return_string="The line is currently: "
+  while counter < katz_deli.count
+    counter+=1
+    return_string+=counter.to_s
+    return_string+=". "
+    return_string+=katz_deli[counter-1]
+    return_string+=" "
+  end
+  puts return_string
 end
 
 def now_serving(katz_deli)
